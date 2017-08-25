5bdb96f9f8502a54134fe19c2a91fa6d5c6549e6
Paul Bodnar <pbodnar@rennie.com>
2017-08-24 23:01:36 -0600

    add save method to Cards

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,6 +1,9 @@
 defmodule Cards do
   def create_deck do
-    values = ["Ace", "Two", "Three", "Four", "Five"]
+    values = [
+      "Ace", "Two", "Three", "Four", "Five", "Six", "Seven",
+      "Eight", "Nine", "Ten", "Jack", "Queen", "King"
+    ]
     suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
 
     cards = for suit <- suits, value <- values do
@@ -15,6 +18,15 @@ defmodule Cards do
 
   def contains?(deck, card) do
     Enum.member?(deck, card)
+  end
+
+  def deal(deck, count) do
+    Enum.split(deck, count)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
   end
 
 end
